83df0c397c60d56dafdb6b5c5d07d536f393dc7e
Lord-AY <ayomidealadesulu@gmail.com>
2021-02-12 12:12:08 +0100

    change

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
+    <Header />
     <Lander />
+    <b-container>
+      <b-row>
+        <b-cols sm="4"></b-cols>
+        <b-cols sm="8"></b-cols>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 import Lander from "@/components/home/Lander.vue";
+import Header from "../components/layout/Header.vue";
 export default {
   components: {
     Lander,
+    Header,
   },
 };
 </script>
