296a1d03201fa95cad6b4b2176a69f9bca1d3f9f
Lord-AY <ayomidealadesulu@gmail.com>
2021-02-16 16:52:57 +0100

    update: footer

---
+++ src/components/layout/FooterComponent.vue
@@ -0,0 +1,153 @@
+<template>
+  <footer
+    class="footer bg-explorerGreen-300 relative pt-1 border-b-2 border-blue-700"
+  >
+    <div class="container mx-auto px-6">
+      <div class="sm:flex sm:mt-8">
+        <div
+          class="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between"
+        >
+          <div class="flex flex-col">
+            <span class="text-explorerGreen-custom mb-2">About us</span>
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >About Trade explorer</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >We are hiring</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >Terms & Conditions</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >Privacy Policy</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >Billing Policy</a
+              ></span
+            >
+          </div>
+          <div class="flex flex-col">
+            <span class="text-explorerGreen-custom mb-2">About us</span>
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >Support</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >support@tdx.com</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >Safety tips</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >Contact us</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >FAQ</a
+              ></span
+            >
+          </div>
+          <div class="flex flex-col">
+            <span class="text-explorerGreen-custom mb-2">Our resources</span>
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >Our blog</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >Our instagram</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >Our twitter</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >Our youtube</a
+              ></span
+            >
+          </div>
+          <div class="flex flex-col">
+            <span class="text-explorerGreen-custom mb-2">Hot links</span>
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >Brand</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >Jiji sellers</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >Regions</a
+              ></span
+            >
+          </div>
+          <!-- <div class="flex flex-col">
+            <span class="text-explorerGreen-custom mb-2">About us</span>
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >About Trade explorer</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >About Trade explorer</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >About Trade explorer</a
+              ></span
+            >
+            <span class="my-2"
+              ><a href="#" class="text-white text-md hover:text-gray-300"
+                >About Trade explorer</a
+              ></span
+            >
+          </div> -->
+        </div>
+      </div>
+    </div>
+    <div class="container mx-auto px-6">
+      <div class="mt-16 flex flex-col items-center">
+        <div class="sm:w-2/3 text-center py-6">
+          <p class="text-sm text-explorerGreen-custom font-bold mb-2">
+            © {{ new Date().getFullYear() }} Trade Explorer
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.text-explorerGreen-custom {
+  color: #279625;
+}
+</style>
