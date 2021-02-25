8e5a9e91dc88d1e8e5367d83b74efbf166634124
Lord-AY <ayomidealadesulu@gmail.com>
2021-02-25 18:53:01 +0100

    update: search -- mobile & desktop

---
+++ src/components/search/MobileSearch.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mt-12 py-8 mx-2">
+    <ProductListing />
+    <ProductListing />
+    <ProductListing />
+    <ProductListing />
+    <ProductListing />
+    <ProductListing />
+  </div>
+</template>
+
+<script>
+import ProductListing from "@/components/search/ProductListing";
+export default {
+  components: { ProductListing },
+};
+</script>
