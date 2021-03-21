48ef04cd93493676bbe28f40bc2f356abf73d13f
Dropcode <classic@Dropcodes-Mac.local>
2021-03-21 16:24:47 +0100

    how to sell mobile

--- src/components/profile/MobileHowtosell.vue
+++ src/components/profile/MobileHowtosell.vue
@@ -130,6 +130,246 @@
           />
         </div>
       </div>
+      <div class="w-10/12 mx-auto block">
+        <div class="my-20">
+          <h2 class="font-bold text-gray-700 text-left tracking-wide text-xl">
+            How to sell on tradex
+          </h2>
+          <p
+            class="mt-6 text-gray-700 text-md w-full rounded px-2 py-2 bg-gray-100"
+          >
+            1. <span class="text-explorerGreen-300 font-bold">Register</span>
+            <br />
+            Register using your e-mail and phone number (or do it via Facebook
+            or Google). Make sure you’re entering a correct phone number, so
+            your clients could reach you!
+          </p>
+          <p
+            class="mt-6 text-gray-700 text-md w-full rounded px-2 py-2 bg-gray-100"
+          >
+            2.<span class="text-gray-800 font-bold"
+              >Make photos of your item.</span
+            >
+            <br />
+            Feel free to make a lot of photos using your smartphone. Make sure
+            they show your item in the best light.
+          </p>
+          <p
+            class="mt-6 text-gray-700 text-md w-full rounded px-2 py-2 bg-gray-100"
+          >
+            3.
+            <span class="text-gray-800 font-bold"
+              >Press <span class="text-explorerGreen-300">Sell</span></span
+            >
+            <br />
+            Choose a proper category, upload your photos and write a clear title
+            and full description of your item. Enter a fair price, select
+            attributes and send your advert to review!
+          </p>
+          <p
+            class="mt-6 text-gray-700 text-md w-full rounded px-2 py-2 bg-gray-100"
+          >
+            4.
+            <span class="text-gray-800 font-bold"
+              >Answer the messages and calls from your clients!</span
+            >
+            <br />
+            If everything is ok with your advert, it’ll be on Jiji in a couple
+            of hours after sending to moderation. We’ll send you a letter and
+            notification when your advert goes live. Check your messages and be
+            ready to earn money! Do you want to sell like a pro? Check out our
+            Premium Services .
+          </p>
+          <div class="text-center mx-auto w-full mt-10">
+            <router-link
+              :to="{ name: 'Home' }"
+              class="mx-auto text-center tracking-wider text-white"
+            >
+              <button
+                class="bg-explorerGreen-300 w-32 font-semibold px-4 py-2 rounded"
+              >
+                Sell
+              </button>
+            </router-link>
+          </div>
+        </div>
+        <div class="w-full">
+          <img
+            src="https://assets.jiji.ng/static/img/about/svg/second.svg"
+            class="w-full h-full"
+            alt=""
+          />
+        </div>
+      </div>
+      <div class="w-11/12 mx-auto block">
+        <div class="my-20 w-full mx-auto">
+          <h2 class="font-bold text-gray-700 text-left tracking-wide text-lg">
+            How to buy on tradex
+          </h2>
+          <p
+            class="mt-6 text-gray-700 text-md w-full bg-gray-100 leading-6 rounded px-4 py-4"
+          >
+            1.
+            <span class="text-explorerGreen-300 font-bold"
+              >Search for the item</span
+            >
+            <br />
+            Find what you need using search panel and filters. We have over a
+            million adverts, choose exactly what you are looking for.
+          </p>
+          <p
+            class="mt-6 text-gray-700 text-md w-full bg-gray-100 leading-6 rounded px-4 py-4"
+          >
+            2.<span class="text-gray-800 font-bold">Contact a seller.</span>
+            <br />
+            You may use chat on Jiji or call them via phone. Discuss all the
+            details, negotiate about the price.
+          </p>
+          <p
+            class="mt-6 text-gray-700 text-md w-full bg-gray-100 leading-6 rounded px-4 py-4"
+          >
+            3.
+            <span class="text-gray-800 font-bold"
+              >Take your item or order a delivery</span
+            >
+            <br />
+            We check our sellers carefully, but it’s always better to check
+            twice, right? Meet a seller in public place and be sure to pay only
+            after collecting your item.
+          </p>
+          <p
+            class="mt-6 text-gray-700 text-md leading-6 w-full bg-gray-100 px-2 py-2 rounded"
+          >
+            4.
+            <span class="text-gray-800 font-bold">
+              Leave your feedback about the seller.</span
+            >
+            <br />
+            Feel free to tell us about your purchase. Your feedback will be
+            published online on the seller’s page and will be very helpful for
+            other buyers. Let’s build a safe and professional business community
+            together!
+          </p>
+          <div class="w-full text-center">
+            <button
+              class="bg-explorerGreen-300 text-center text-white tracking-wide font-semibold px-2 py-2 rounded my-6"
+            >
+              Go Shopping
+            </button>
+          </div>
+        </div>
+        <div class="w-full">
+          <img
+            src="https://assets.jiji.ng/static/img/about/svg/third.svg"
+            class="w-full h-full"
+            alt=""
+          />
+        </div>
+      </div>
+      <div class="w-11/12 mx-auto block">
+        <div class="my-20 w-full">
+          <h2
+            class="font-bold my-6 text-gray-700 text-left tracking-wide text-lg"
+          >
+            Safety
+          </h2>
+          <p
+            class="text-gray-700 leading-6 text-left tracking-wide text-md bg-gray-100 px-4 py-4 w-full rounded"
+          >
+            1.
+            <span class="text-explorerGreen-300 font-bold">General</span>
+            <br />
+            We are highly focused on the security and can solve any issues in
+            short-terms. That’s why we ask you, kindly, to leave a review after
+            purchasing. If you run into any problems with a seller, you can
+            report us and Jiji Team will check this seller as soon as possible.
+          </p>
+          <p
+            class="text-gray-700 leading-6 text-left tracking-wide text-md bg-gray-100 px-4 py-4 w-full rounded my-6"
+          >
+            2.<span class="text-gray-800 font-bold">Personal safety tips.</span>
+            <br />
+            - Do not pay in advance, even for the delivery - Try to meet at a
+            safe, public location - Check the item BEFORE you buy it - Pay only
+            after collecting the item
+          </p>
+          <p
+            class="text-gray-700 leading-6 text-left tracking-wide text-md bg-gray-100 px-4 py-4 w-full rounded my-6"
+          >
+            3.
+            <span class="text-gray-800 font-bold">Secure Payments</span>
+            <br />
+            tradex provides Premium Services for those who want to sell and earn
+            more. We accept both online and offline payments for these services.
+            We guarantee secure and reliable payments on tradex.
+          </p>
+        </div>
+        <div class="w-full">
+          <img
+            src="https://assets.jiji.ng/static/img/about/svg/fourth.svg"
+            class="w-full h-full"
+            alt=""
+          />
+        </div>
+      </div>
+      <div class="w-11/12 mx-auto block">
+        <div class="my-20 w-full">
+          <h2 class="font-bold text-gray-700 text-left tracking-wide text-lg">
+            Sell like a pro
+          </h2>
+          <p
+            class="mt-6 text-gray-700 text-md w-full bg-gray-100 leading-6 rounded px-4 py-4"
+          >
+            1.
+            <span class="text-explorerGreen-300 font-bold"
+              >Pay attention to details</span
+            >
+            <br />
+            Make good photos of your goods, write clear and detailed
+            description.
+          </p>
+          <p
+            class="mt-6 text-gray-700 text-md w-full bg-gray-100 leading-6 rounded px-4 py-4"
+          >
+            2.<span class="text-gray-800 font-bold">Answer Quickly.</span>
+            <br />
+            Don’t make your buyer wait for your message for days. Be online or
+            get SMS notifications on your messages.
+          </p>
+          <p
+            class="mt-6 text-gray-700 text-md w-full bg-gray-100 leading-6 rounded px-4 py-4"
+          >
+            3.
+            <span class="text-gray-800 font-bold"
+              >Use Premium Services to get 15x more customers!</span
+            >
+            <br />
+            Your adverts will appear at the top of the page and you will sell
+            faster! How does it work?
+          </p>
+          <div class="text-center w-full">
+            <button
+              class="bg-explorerGreen-300 text-white tracking-wide w-auto px-2 py-2 rounded my-6"
+            >
+              Go Premium
+            </button>
+          </div>
+        </div>
+        <div class="w-full">
+          <img
+            src="https://assets.jiji.ng/static/img/about/svg/fifth.svg"
+            class="w-full h-full"
+            alt=""
+          />
+        </div>
+      </div>
+      <div class="w-10/12 mx-auto text-center my-6">
+        <button
+          class="mx-auto bg-gray-200 rounded text-gray-800 tracking-tight font-semibold w-auto px-3 py-2"
+        >
+          Still have questions? Read FAQ
+        </button>
+      </div>
     </div>
     <!-- Mobile how to sell content -->
   </div>
