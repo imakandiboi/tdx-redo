031139c97040af52e0fc66d0e41349a19bbbfc6f
chiagozie Arinzechi <imakandiboi@yahoo.com>
2021-03-26 23:29:13 +0100

    commit

--- src/components/plan/Property.vue
+++ src/components/plan/Property.vue
@@ -3,8 +3,6 @@
     <div class="h-100 bg-white">
       <h1>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id quaerat,
-        porro quos nemo vitae corrupti dicta enim atque quisquam laboriosam
-        explicabo sunt saepe error fugit magni rerum repellat distinctio omnis.
       </h1>
       <router-view></router-view>
     </div>
